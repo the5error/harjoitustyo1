7c61fd0cbe19b51298f7e398f7cbe171123879b3
Konsta Toivonen <the5error@gmail.com>
2019-02-04 10:27:31 +0200

    Added Tekstia & TulostaTekstia

---
+++ src/TulostaTekstia.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+import Tekstia from "./components/Tekstia"
+class test extends Component {
+    render() {
+        return(
+          <div>
+          <Tekstia />
+          </div>
+        );
+    }
+}
+export default test;
---
+++ src/components/Tekstia.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+class Tekstia extends Component {
+ render() {
+  return (
+   <div>
+     <h3>React</h3>
+     <p>Reactin perusteita</p>
+</div> );
+} }
+export default Tekstia;
